88ddbb06e8864c7b4d9f3e603d520fec18dec4df
KowalczykEmil <pomocyakoshi@gmail.com>
2022-03-02 00:41:25 +0100

    Created project

---
+++ src/main/java/tech/getarrays/privatelessons/PrivatelessonsApplication.java
@@ -0,0 +1,13 @@
+package tech.getarrays.privatelessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatelessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivatelessonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/getarrays/privatelessons/PrivatelessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.getarrays.privatelessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivatelessonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
