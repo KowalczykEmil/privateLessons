abb4db00b70df1b05c8818315e8109dfbd8300af
KowalczykEmil <pomocyakoshi@gmail.com>
2022-03-06 19:24:03 +0100

    Created tutorsRepo interface

--- src/main/java/tech/getarrays/privatelessons/repo/TutorsRepo.java
+++ src/main/java/tech/getarrays/privatelessons/repo/TutorsRepo.java
@@ -1,2 +1,12 @@
-package tech.getarrays.privatelessons.repo;public interface TutorsRepo {
+package tech.getarrays.privatelessons.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.getarrays.privatelessons.model.Tutors;
+
+import java.util.Optional;
+
+public interface TutorsRepo extends JpaRepository<Tutors, Long> {
+    void deleteTutorsById(Long id);
+
+    Optional<Tutors> findTutorsById(Long id);
 }
